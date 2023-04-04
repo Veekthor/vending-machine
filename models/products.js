ac0840a647556163acf3e16aba20052e1d95a3b2
veekthor <chukailozulu@gmail.com>
2023-04-04 23:30:12 +0100

    1. Added swagger docs 2. Implement user CRUD docs

--- models/products.js
+++ models/products.js
@@ -1,6 +1,41 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Product:
+ *        type: object
+ *        required:
+ *          - cost
+ *          - productName
+ *          - sellerId
+ *        properties:
+ *          _id:
+ *            type: string
+ *            description: object ID in DB(set by mongoose).
+ *          amountAvailable:
+ *            type: integer
+ *            minimum: 0
+ *            maximum: 500
+ *            default: 0
+ *            description: Amount of product items available
+ *          cost:
+ *            type: integer
+ *            minimum: 0
+ *            maximum: 1,000,000
+ *            multipleOf: 5
+ *            description: Cost of product; must be multiple of 5
+ *          productName:
+ *            type: string
+ *            minlength: 3
+ *            maxlength: 20
+ *            description: Product's name
+ *          sellerId:
+ *            type: string
+ *            description: object ID of seller in DB
+ */
 const productSchema = new Schema({
     amountAvailable: {
         type: Number,
--- models/users.js
+++ models/users.js
@@ -2,6 +2,121 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        required:
+ *          - username
+ *          - password
+ *          - role
+ *        properties:
+ *          _id:
+ *            type: string
+ *            description: object ID in DB(set by mongoose).
+ *          username:
+ *            type: string
+ *            minlength: 5
+ *            maxlength: 20
+ *            description: Username
+ *          password:
+ *            type: string
+ *            minlength: 5
+ *            maxlength: 100
+ *            description: Password
+ *          deposit:
+ *            type: integer
+ *            minimum: 0
+ *            maximum: 1,000,000
+ *            default: 0
+ *            description: User's deposit
+ *          role:
+ *            type: string
+ *            enum: [buyer, seller]
+ *            default: buyer
+ *            description: User's role
+ *        example:
+ *          _id: 2345654cdhhw345dnchd4583c
+ *          name: John Doe
+ *          password: 12345678
+ *          deposit: 255
+ *          role: buyer
+ */
+
+/**
+ * @swagger
+ *  components:
+ *    requestBodies:
+ *      UserInput:
+ *        required: true
+ *        content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *               - role
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 minlength: 5
+ *                 maxlength: 20
+ *                 description: Username
+ *               password:
+ *                 type: string
+ *                 minlength: 5
+ *                 maxlength: 100
+ *                 description: Password
+ *               role:
+ *                 type: string
+ *                 enum: [buyer, seller]
+ *                 default: buyer
+ *                 description: User's role
+ *             example:
+ *               username: test123
+ *               password: testpass
+ *               role: seller
+ */
+
+/**
+ * @swagger
+ * components:
+ *   responses:
+ *     UserResponse:
+ *       required: true
+ *       content:
+ *         application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - username
+ *              - deposit
+ *              - role
+ *            properties:
+ *              username:
+ *                type: string
+ *                minlength: 5
+ *                maxlength: 20
+ *                description: Username
+ *              deposit:
+ *                type: integer
+ *                default: 0
+ *                maximum: 1,000,000
+ *                description: User's deposit
+ *              role:
+ *                type: string
+ *                enum: [buyer, seller]
+ *                default: buyer
+ *                description: User's role
+ *            example:
+ *              username: test123
+ *              deposit: 300
+ *              role: seller
+ */
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
