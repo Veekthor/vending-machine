25a6a123666dcbb4b079dd054bf5ef40aecb8218
veekthor <chukailozulu@gmail.com>
2023-06-16 14:18:21 +0100

    Added health route and helmet

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 require("dotenv").config();
+const helmet = require("helmet");
 const express = require("express");
 const app = express();
 
 require("./routes")(app);
 require("./db")();
+
 app.get("/", (req, res) => {
-    res.json({
-        health: "OK"
-    })
-})
+  res.redirect("/docs");
+});
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    health: "OK",
+  });
+});
+
+if (process.env.NODE_ENV === "production") app.use(helmet());
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-})
+  console.log(`Listening on port ${port}...`);
+});
 
 module.exports = server;
